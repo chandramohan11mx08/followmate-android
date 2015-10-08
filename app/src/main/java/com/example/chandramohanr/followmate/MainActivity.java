16b20a87d3593304dae7af9ed4cec978ba8fc8c9
Chandramohan R <chandra@stayzilla.com>
2015-10-08 17:32:18 +0530

    chandra: Initial project setup

---
+++ app/src/main/java/com/example/chandramohanr/followmate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chandramohanr.followmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
