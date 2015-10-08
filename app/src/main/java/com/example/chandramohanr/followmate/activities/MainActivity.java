a155ce85586be648fe5d34c308fda739a4c592a5
Chandramohan R <chandra@stayzilla.com>
2015-10-08 18:59:23 +0530

    chandra: add map fragment into MainActivity

---
+++ app/src/main/java/com/example/chandramohanr/followmate/activities/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.chandramohanr.followmate.activities;
+
+import com.example.chandramohanr.followmate.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends BaseActivity implements OnMapReadyCallback {
+
+    @AfterViews
+    void afterViewInjection() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
