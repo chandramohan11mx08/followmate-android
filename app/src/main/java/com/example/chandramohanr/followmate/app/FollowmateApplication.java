e309b8caad6ffcd283b7c8356f4cbc8b494d5c70
Chandramohan R <chandra@stayzilla.com>
2015-10-15 12:07:03 +0530

    chandra: add FollomateApplication and SharedPrefernceHelper

---
+++ app/src/main/java/com/example/chandramohanr/followmate/app/FollowmateApplication.java
@@ -0,0 +1,22 @@
+package com.example.chandramohanr.followmate.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.noveogroup.android.log.Log;
+
+public class FollowMateApplication extends Application{
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mContext = getApplicationContext();
+        Log.a("Application started");
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+}
---
+++ app/src/main/java/com/example/chandramohanr/followmate/app/helpers/SharedPreferenceHelper.java
@@ -0,0 +1,75 @@
+package com.example.chandramohanr.followmate.app.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.chandramohanr.followmate.app.FollowMateApplication;
+
+public class SharedPreferenceHelper {
+    public static final String FOLLOWMATE_SHARED_PREF = "com.follomate.app";
+
+    public static final String KEY_USER_ID = "uid";
+
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+
+    private static void openSharedPreferencesInEditMode() {
+        editor = FollowMateApplication.getContext().
+                getSharedPreferences(FOLLOWMATE_SHARED_PREF, Context.MODE_PRIVATE).edit();
+    }
+
+    private static void openSharedPreferencesInReadMode() {
+        sharedPreferences = FollowMateApplication.getContext().
+                getSharedPreferences(FOLLOWMATE_SHARED_PREF, Context.MODE_PRIVATE);
+    }
+
+    private static void closeSharedPreferences() {
+        editor.commit();
+    }
+
+    public static void set(String key, String value) {
+        openSharedPreferencesInEditMode();
+        editor.putString(key, value);
+        closeSharedPreferences();
+    }
+
+    public static String getString(String key) {
+        openSharedPreferencesInReadMode();
+        return sharedPreferences.getString(key, "");
+    }
+
+    public static void set(String key, int value) {
+        openSharedPreferencesInEditMode();
+        editor.putInt(key, value);
+        closeSharedPreferences();
+    }
+
+    public static void set(String key, long value) {
+        openSharedPreferencesInEditMode();
+        editor.putLong(key, value);
+        closeSharedPreferences();
+    }
+
+    public static void set(String key, boolean value) {
+        openSharedPreferencesInEditMode();
+        editor.putBoolean(key, value);
+        closeSharedPreferences();
+    }
+
+    public static boolean getBoolean(String key) {
+        openSharedPreferencesInReadMode();
+        return sharedPreferences.getBoolean(key, false);
+    }
+
+    public static void clearSharedPreferencesData() {
+        openSharedPreferencesInEditMode();
+        editor.clear();
+        closeSharedPreferences();
+    }
+
+    public static void deleteSharedPreference(String key) {
+        openSharedPreferencesInEditMode();
+        editor.remove(key);
+        closeSharedPreferences();
+    }
+}
